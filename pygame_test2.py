f82738be7398b5458ca64166cf10313b03d6456b
Luke Hackworth <lukeh@evrichart.com>
2015-11-25 15:35:22 -0600

    Add GPL Notice

--- pygame_test2.py
+++ pygame_test2.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python2.7
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  Originally made by Luke Hackworth
 
 import sys, os
 import math
@@ -108,6 +123,5 @@ def screenUpdate():
 while True:
 	for event in pygame.event.get():
 		pass
-	#print pygame.mouse.get_pos()
 	main()
-	screenUpdate() #draws everything(be sure to put what to render in there)
+	screenUpdate() #draws everything
