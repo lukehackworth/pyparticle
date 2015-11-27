680539ba75a17e496bcc04696864e348fc733f8c
Luke Hackworth <lukeh@evrichart.com>
2015-11-27 09:28:18 -0600

    Replace tabs with spaces.
    This aligns with the python standards better.

--- pygame_test2.py
+++ pygame_test2.py
@@ -41,94 +41,94 @@ max_dist = 7
 particle_array = []
 
 class Particle:
-	loc = []
+    loc = []
 
 def main():
-	global loc_particle_2
-	mouseCheck()
+    global loc_particle_2
+    mouseCheck()
 
-	particle_count = 0
-	#very greedy
-	for part_a in particle_array:
-		for part_b in particle_array:
-			if(part_a.loc != part_b.loc):
-				part_dist = locsDistCalc(part_a.loc, part_b.loc)
-				if(part_dist < max_dist):
-					j = particleBoundsCheck(part_a.loc, part_b.loc, part_dist)
-					part_a.loc = j[0]
-					part_b.loc = j[1]
+    particle_count = 0
+    #very greedy
+    for part_a in particle_array:
+        for part_b in particle_array:
+            if(part_a.loc != part_b.loc):
+                part_dist = locsDistCalc(part_a.loc, part_b.loc)
+                if(part_dist < max_dist):
+                    j = particleBoundsCheck(part_a.loc, part_b.loc, part_dist)
+                    part_a.loc = j[0]
+                    part_b.loc = j[1]
 
-	particle_count += 1
+    particle_count += 1
 
 def mouseCheck():
-	is_mouse_pressed = pygame.mouse.get_pressed()[0]
-	
-	#Adds particle
-	if(is_mouse_pressed):
-		mouse_loc = pygame.mouse.get_pos()
-		#pygame.mouse.get_pos is a tuple(immutable), must be changed to array before assigning to particle so particle loc can be updated
-		mouse_loc_array = [mouse_loc[0], mouse_loc[1]]
-		#Random float added to locs so no two particles have same coordinates
-		m = Particle()
-		m.loc = [mouse_loc_array[0]+random.random(), mouse_loc_array[1]+random.random()]
-		particle_array.append(m)
+    is_mouse_pressed = pygame.mouse.get_pressed()[0]
+    
+    #Adds particle
+    if(is_mouse_pressed):
+        mouse_loc = pygame.mouse.get_pos()
+        #pygame.mouse.get_pos is a tuple(immutable), must be changed to array before assigning to particle so particle loc can be updated
+        mouse_loc_array = [mouse_loc[0], mouse_loc[1]]
+        #Random float added to locs so no two particles have same coordinates
+        m = Particle()
+        m.loc = [mouse_loc_array[0]+random.random(), mouse_loc_array[1]+random.random()]
+        particle_array.append(m)
 
 def particleBoundsCheck(part_a, part_b, part_dist):
-	#TODO: Add particle edge bounds here, too
-	part_delta = particleDeltaFind(part_a, part_b)
-	m = 0.5 * (max_dist - part_dist)
-	delta_x_a = (m*part_delta[0])/part_dist
-	delta_y_a = (m*part_delta[1])/part_dist
-	part_b[0] += delta_x_a
-	part_b[1] += delta_y_a
-	part_a[0] -= delta_x_a
-	part_a[1] -= delta_y_a
-	return [part_a,part_b]
+    #TODO: Add particle edge bounds here, too
+    part_delta = particleDeltaFind(part_a, part_b)
+    m = 0.5 * (max_dist - part_dist)
+    delta_x_a = (m*part_delta[0])/part_dist
+    delta_y_a = (m*part_delta[1])/part_dist
+    part_b[0] += delta_x_a
+    part_b[1] += delta_y_a
+    part_a[0] -= delta_x_a
+    part_a[1] -= delta_y_a
+    return [part_a,part_b]
 
 def particleCorrectLocFind(particle_a, particle_b):
-	#base_particle_loc = particle_a
-	#loc_particle_2 = particle_b
-	particle_delta = particleDeltaFind(particle_a, loc_particle_2)
-	c = math.sqrt(particle_delta[0]**2 + particle_delta[1]**2)
-	
-	correct_delta_x = (particle_delta[0]*max_dist)/c
-	correct_delta_y = (particle_delta[1]*max_dist)/c
-		
-	#offset delta from base_particle_loc
-	correct_x = base_particle_loc[0] + correct_delta_x
-	correct_y = base_particle_loc[1] + correct_delta_y
-	
-	correct_loc = (correct_x, correct_y)
-	return correct_loc
-	
+    #base_particle_loc = particle_a
+    #loc_particle_2 = particle_b
+    particle_delta = particleDeltaFind(particle_a, loc_particle_2)
+    c = math.sqrt(particle_delta[0]**2 + particle_delta[1]**2)
+    
+    correct_delta_x = (particle_delta[0]*max_dist)/c
+    correct_delta_y = (particle_delta[1]*max_dist)/c
+        
+    #offset delta from base_particle_loc
+    correct_x = base_particle_loc[0] + correct_delta_x
+    correct_y = base_particle_loc[1] + correct_delta_y
+    
+    correct_loc = (correct_x, correct_y)
+    return correct_loc
+    
 def particleDeltaFind((x1, y1), (x2, y2)):
-	delta_x = x2 - x1
-	delta_y = y2 - y1
-	return [delta_x, delta_y]
+    delta_x = x2 - x1
+    delta_y = y2 - y1
+    return [delta_x, delta_y]
 
 def locsDistCalc(a, b):
-	x1 = a[0]
-	y1 = a[1]
-	x2 = b[0]
-	y2 = b[1]
-	answer = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-	return answer
+    x1 = a[0]
+    y1 = a[1]
+    x2 = b[0]
+    y2 = b[1]
+    answer = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return answer
 
 def screenUpdate():
-	screen.fill(background_color)
-	
-	for particle in particle_array:
-		pygame.draw.circle(screen, black, (int(particle.loc[0]), int(particle.loc[1])), 5, 2)
-	
-	pygame.display.update()
-	
-	
-#main loop	
+    screen.fill(background_color)
+    
+    for particle in particle_array:
+        pygame.draw.circle(screen, black, (int(particle.loc[0]), int(particle.loc[1])), 5, 2)
+    
+    pygame.display.update()
+    
+    
+#main loop  
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		pass
-	main()
-	screenUpdate() #draws everything
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        pass
+    main()
+    screenUpdate() #draws everything
