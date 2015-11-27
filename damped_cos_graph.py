79be6ad5b1339bf0b88bf45f304e9bf28020c5a1
Luke Hackworth <lukehackworth@gmail.com>
2015-11-27 14:42:04 -0600

    Add desired_max_dist variable

--- damped_cos_graph.py
+++ damped_cos_graph.py
@@ -10,17 +10,19 @@ decay_const = 7
 offset = 500
 angular_frequency = 1
 phase_angle = 1
+desired_max_dist = 28
 
 #X generator
 x_count = 200
-x_diff = 0.00375
+x_diff = 0.105
 
 for j in range(x_count):
     sink = j*x_diff
     x.append(sink)
     print sink
 
-for mink in x:
+for l in x:
+    mink = l * 1/desired_max_dist
     soon = math.e**(-(mink))*math.cos(2 * math.pi * mink)
     y.append(soon)
 
