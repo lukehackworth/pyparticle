9742548d38e48caf2a12cb07b6f6d61de8aea80e
luke <luke@hackstation.hackdomain>
2016-03-27 15:22:58 -0500

    Lint

--- pygame_test2.py
+++ pygame_test2.py
@@ -3,52 +3,56 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  Originally made by Luke Hackworth
 
-import sys, os
+import sys
+import os
 import math
 import pygame
-import time #for time.sleep testing, can be removed after testing
-import random #for rand float entry so (hopefully) no two particle locations are the same
+import time
+import random
 from pygame.locals import *
 pygame.init()
 
-# for reference see https://www.cs.ucsb.edu/~pconrad/cs5nm/topics/pygame/drawing/
+# for reference see
+# https://www.cs.ucsb.edu/~pconrad/cs5nm/topics/pygame/drawing/
 
-##Variable initialization
-#screen
+# Variable initialization
+# screen
 size = width, height = 640, 480
 screen = pygame.display.set_mode(size)
-#colors
+# colors
 black = (0, 0, 0)
 background_color = (100, 150, 50)
-#particle variables
-base_particle_loc = (width/2,height/2)
+# particle variables
+base_particle_loc = (width/2, height/2)
 max_dist = 7
 particle_array = []
 
+
 class Particle:
     def __init__(self):
-        self.vel = [0,0]
+        self.vel = [0, 0]
     loc = []
     vel = []
+
 
 def main():
     mouseCheck()
 
-    #very greedy
+    # very greedy
     for part_a in particle_array:
-        part_a.vel[1] +=0.02
+        part_a.vel[1] += 0.02
         for part_b in particle_array:
             if(part_a.loc != part_b.loc):
                 part_dist = locsDistCalc(part_a.loc, part_b.loc)
@@ -58,21 +62,29 @@ def main():
                     part_b.loc = j[1]
         part_a.loc[1] += part_a.vel[1]
 
+
 def mouseCheck():
     is_mouse_pressed = pygame.mouse.get_pressed()[0]
-    
-    #Adds particle
+
+    # Adds particle
     if(is_mouse_pressed):
         mouse_loc = pygame.mouse.get_pos()
-        #pygame.mouse.get_pos is a tuple(immutable), must be changed to array before assigning to particle so particle loc can be updated
+        # pygame.mouse.get_pos is a tuple(immutable), must be changed to
+        # array before assigning to particle so particle loc can be
+        # updated
         mouse_loc_array = [mouse_loc[0], mouse_loc[1]]
-        #Random float added to locs so no two particles have same coordinates
+        # Random float added to locs so no two particles have same
+        # coordinates
         m = Particle()
-        m.loc = [mouse_loc_array[0]+random.random(), mouse_loc_array[1]+random.random()]
+        m.loc = [
+            mouse_loc_array[0]+random.random(),
+            mouse_loc_array[1]+random.random()
+        ]
         particle_array.append(m)
 
+
 def particleBoundsCheck(part_a, part_b, part_dist):
-    #TODO: Add particle edge bounds here, too
+    # TODO: Add particle edge bounds here, too
     part_delta = particleDeltaFind(part_a, part_b)
     m = 0.5 * (max_dist - part_dist)
     delta_x_a = (m*part_delta[0])/part_dist
@@ -81,26 +93,29 @@ def particleBoundsCheck(part_a, part_b, part_dist):
     part_b[1] += delta_y_a
     part_a[0] -= delta_x_a
     part_a[1] -= delta_y_a
-    return [part_a,part_b]
+    return [part_a, part_b]
+
 
 def particleCorrectLocFind(particle_a, particle_b):
     particle_delta = particleDeltaFind(particle_a, loc_particle_2)
     c = math.sqrt(particle_delta[0]**2 + particle_delta[1]**2)
-    
+
     correct_delta_x = (particle_delta[0]*max_dist)/c
     correct_delta_y = (particle_delta[1]*max_dist)/c
-        
-    #offset delta from base_particle_loc
+
+    # offset delta from base_particle_loc
     correct_x = base_particle_loc[0] + correct_delta_x
     correct_y = base_particle_loc[1] + correct_delta_y
-    
+
     correct_loc = (correct_x, correct_y)
     return correct_loc
-    
+
+
 def particleDeltaFind((x1, y1), (x2, y2)):
     delta_x = x2 - x1
     delta_y = y2 - y1
     return [delta_x, delta_y]
+
 
 def locsDistCalc(a, b):
     x1 = a[0]
@@ -110,14 +125,20 @@ def locsDistCalc(a, b):
     answer = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     return answer
 
+
 def screenUpdate():
     screen.fill(background_color)
-    
+
     for particle in particle_array:
-        pygame.draw.circle(screen, black, (int(particle.loc[0]), int(particle.loc[1])), 5, 2)
-    
+        pygame.draw.circle(
+            screen, black,
+            (int(particle.loc[0]), int(particle.loc[1])),
+            5, 2
+        )
+
     pygame.display.update()
-    
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -128,4 +149,4 @@ while True:
                 particle_array = []
         pass
     main()
-    screenUpdate() #draws everything
+    screenUpdate()  # draws everything
